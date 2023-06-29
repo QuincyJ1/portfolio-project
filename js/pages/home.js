fdfd82b99d193bc1e2946b8b9538b15b4d4ed946
Dainius <dainiusnarkevicius1@gmail.com>
2023-06-29 11:24:04 +0300

    update

---
+++ js/pages/home.js
@@ -0,0 +1,18 @@
+// IMPORT
+
+// EXECUTION
+
+/* header start */
+/* header end */
+
+/* about start */
+const h1DOM = document.getElementsByTagName('h1')[0]; // is to masyvo paimi pirma elementa, kuris yra h1 musu tekstas. 
+h1DOM.innerText = 'Labas vsakaras';
+
+/* about end */
+
+/* services start */
+/* services end */
+
+/* footer start */
+/* footer end */
